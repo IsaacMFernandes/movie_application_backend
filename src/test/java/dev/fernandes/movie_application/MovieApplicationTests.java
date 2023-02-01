5c9b6413caaab294ef55f689a267d089d59fa878
Isaac Fernandes <code.isaacfernandes@gmail.com>
2023-01-31 19:32:46 -0500

    Initial commit of a basic backend setup with a MongoDB cluster integrated.

---
+++ src/test/java/dev/fernandes/movie_application/MovieApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.fernandes.movie_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests
+{
+    
+    @Test
+    void contextLoads()
+    {
+    }
+    
+}
